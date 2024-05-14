2f58984aad78bfad248261618f2cb98dc6ff8208
evavashisth <evavashisth96@gmail.com>
2024-05-13 22:53:19 -0700

    Added a few optimizations to two screens.

--- lib/screens/educationalResources/educationalResourcesScreen.dart
+++ lib/screens/educationalResources/educationalResourcesScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// This is the main widget for the Educational Resources screen
 class EducationalResourcesScreen extends StatefulWidget {
   const EducationalResourcesScreen({Key? key}) : super(key: key);
 
@@ -8,8 +9,10 @@ class EducationalResourcesScreen extends StatefulWidget {
       _EducationalResourcesScreenState();
 }
 
+// This is the state class for the Educational Resources screen
 class _EducationalResourcesScreenState
     extends State<EducationalResourcesScreen> {
+  // This list contains the educational resource information
   List<Map<String, String>> resources = [
     {
       'title': 'Healthy Eating Habits',
@@ -29,19 +32,24 @@ class _EducationalResourcesScreenState
     // Add more resources as needed
   ];
 
+  // This is the build method that creates the UI for the screen
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Set the app bar title
       appBar: AppBar(
         title: Text('Educational Resources'),
       ),
+      // Use a ListView.builder to display the list of resources
       body: ListView.builder(
         itemCount: resources.length,
         itemBuilder: (context, index) {
           return Card(
             child: ListTile(
+              // Display the title and description of the resource
               title: Text(resources[index]['title']!),
               subtitle: Text(resources[index]['description']!),
+              // Add an onTap callback to handle tapping on a resource
               onTap: () {
                 // Navigate to a detailed resource page or show more information
               },
