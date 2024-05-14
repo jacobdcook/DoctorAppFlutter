60be33ac7e1eb8484e2c108d767ed2c359960e2b
evavashisth <evavashisth96@gmail.com>
2024-05-14 00:05:45 -0700

    Enhance code readability and maintainability with detailed comments

--- lib/screens/patientInfo/addPrescription.dart
+++ lib/screens/patientInfo/addPrescription.dart
@@ -4,21 +4,23 @@ import 'package:flutter/material.dart';
 class AddPrescriptionScreen extends StatefulWidget {
   final Map<String, dynamic>? patient;
 
-  const AddPrescriptionScreen({Key? key, required this.patient}) : super(key: key);
+  const AddPrescriptionScreen({Key? key, required this.patient})
+      : super(key: key);
 
   @override
   _AddPrescriptionScreenState createState() => _AddPrescriptionScreenState();
 }
 
 class _AddPrescriptionScreenState extends State<AddPrescriptionScreen> {
-  final _formKey = GlobalKey<FormState>();                          
-  final _titleController = TextEditingController();    
-  final _dosageController = TextEditingController();   
-  final _startDateController = TextEditingController();           
-  final _endDateController = TextEditingController();           
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _dosageController = TextEditingController();
+  final _startDateController = TextEditingController();
+  final _endDateController = TextEditingController();
 
   @override
   void dispose() {
+    // Dispose the text editing controllers when the widget is disposed
     _titleController.dispose();
     _dosageController.dispose();
     _startDateController.dispose();
@@ -29,21 +31,32 @@ class _AddPrescriptionScreenState extends State<AddPrescriptionScreen> {
   Future<void> _addPrescription() async {
     if (_formKey.currentState!.validate()) {
       try {
-        // All boxes need to be filled to successfully add a prescription
-        if (_titleController.text == "" || _dosageController.text == "" || _startDateController.text == "" || _endDateController == "") {
+        // Check if all fields are filled before adding a prescription
+        if (_titleController.text == "" ||
+            _dosageController.text == "" ||
+            _startDateController.text == "" ||
+            _endDateController == "") {
+          // Show a snackbar message if any field is empty
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
               content: Text('Please fill in all areas'),
             ),
           );
         } else {
-          await FirebaseFirestore.instance.collection('patients').doc("${widget.patient?['fName']}${widget.patient?['mName']}${widget.patient?['lName']}").collection('prescriptions').add({
+          // Add the prescription to Firestore
+          await FirebaseFirestore.instance
+              .collection('patients')
+              .doc(
+                  "${widget.patient?['fName']}${widget.patient?['mName']}${widget.patient?['lName']}")
+              .collection('prescriptions')
+              .add({
             'title': _titleController.text,
             'dosage': _dosageController.text,
             'startDate': _startDateController.text,
             'endDate': _endDateController.text,
           });
 
+          // Show a success message using a snackbar
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
               content: Text('Prescription added successfully'),
@@ -51,7 +64,7 @@ class _AddPrescriptionScreenState extends State<AddPrescriptionScreen> {
           );
         }
       } catch (e) {
-        // Handle any errors that occurred during registration
+        // Handle any errors that occurred during the process
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text('Failed to add prescription'),
@@ -73,23 +86,33 @@ class _AddPrescriptionScreenState extends State<AddPrescriptionScreen> {
           key: _formKey,
           child: ListView(
             children: [
+              // Text field for prescription title
               TextFormField(
                 controller: _titleController,
-                decoration: InputDecoration(labelText: 'Title', labelStyle: TextStyle(fontSize: 14)),
+                decoration: InputDecoration(
+                    labelText: 'Title', labelStyle: TextStyle(fontSize: 14)),
               ),
+              // Text field for prescription dosage
               TextFormField(
                 controller: _dosageController,
-                decoration: InputDecoration(labelText: 'Dosage', labelStyle: TextStyle(fontSize: 14)),
+                decoration: InputDecoration(
+                    labelText: 'Dosage', labelStyle: TextStyle(fontSize: 14)),
               ),
+              // Text field for prescription start date
               TextFormField(
                 controller: _startDateController,
-                decoration: InputDecoration(labelText: 'Start Date', labelStyle: TextStyle(fontSize: 14)),
+                decoration: InputDecoration(
+                    labelText: 'Start Date',
+                    labelStyle: TextStyle(fontSize: 14)),
               ),
+              // Text field for prescription end date
               TextFormField(
                 controller: _endDateController,
-                decoration: InputDecoration(labelText: 'End Date', labelStyle: TextStyle(fontSize: 14)),
+                decoration: InputDecoration(
+                    labelText: 'End Date', labelStyle: TextStyle(fontSize: 14)),
               ),
               SizedBox(height: 16.0),
+              // Button to add the prescription
               ElevatedButton(
                 onPressed: _addPrescription,
                 child: Text('Add Prescription'),
